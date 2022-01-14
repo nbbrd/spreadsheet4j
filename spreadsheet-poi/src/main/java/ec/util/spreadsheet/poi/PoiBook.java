b91e6817d372d0c97afe8cf32690af02a552f4a8
Philippe Charles <philippe.charles@nbb.be>
2022-01-14 10:25:00 +0100

    Fix invalid file lock

--- spreadsheet-poi/src/main/java/ec/util/spreadsheet/poi/PoiBook.java
+++ spreadsheet-poi/src/main/java/ec/util/spreadsheet/poi/PoiBook.java
@@ -1,27 +1,24 @@
 /*
  * Copyright 2013 National Bank of Belgium
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
  *
  * http://ec.europa.eu/idabc/eupl
  *
- * Unless required by applicable law or agreed to in writing, software 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package ec.util.spreadsheet.poi;
 
 import ec.util.spreadsheet.Book;
 import ec.util.spreadsheet.Sheet;
-import java.io.EOFException;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import ec.util.spreadsheet.helpers.ArrayBook;
 import lombok.AccessLevel;
 import org.apache.poi.EmptyFileException;
 import org.apache.poi.UnsupportedFileFormatException;
@@ -36,18 +33,29 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import shaded.spreadsheet.nbbrd.io.sys.OS;
+
+import java.io.*;
 
 /**
- *
  * @author Philippe Charles
  */
 @lombok.AllArgsConstructor(access = AccessLevel.PRIVATE)
 final class PoiBook extends Book {
 
     @NonNull
-    public static PoiBook create(@NonNull File file) throws IOException {
+    public static Book create(@NonNull File file) throws IOException {
         try {
-            return new PoiBook(new XSSFWorkbook(OPCPackage.open(file.getPath(), PackageAccess.READ)));
+            // Fix strange bug on Windows+Java8 that keeps a handle on invalid file
+            if (OS.Name.WINDOWS.equals(OS.NAME)) {
+                try (InputStream stream = new FileInputStream(file)) {
+                    try (PoiBook book = new PoiBook(new XSSFWorkbook(OPCPackage.open(stream)))) {
+                        return ArrayBook.copyOf(book);
+                    }
+                }
+            }
+
+            return new PoiBook(new XSSFWorkbook(OPCPackage.open(file, PackageAccess.READ)));
         } catch (OpenXML4JException | OpenXML4JRuntimeException | UnsupportedFileFormatException ex) {
             throw new IOException(file.getPath(), ex);
         } catch (EmptyFileException ex) {
